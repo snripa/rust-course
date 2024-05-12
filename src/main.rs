70d54893a3ed5b8c7b5bce0be6f162d3029a7978
Sergii Ripa <sergey@exberry.io>
2024-05-12 11:00:47 +0200

    Init guess game

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,6 @@ fn main() {
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1..100);
-    println!("secret_number = {secret_number}");
-
     loop {
         println!("Please input your guess > ");
         let mut guess = String::new();
@@ -16,14 +14,17 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = guess.trim().parse().expect("please type a number");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
             Ordering::Equal => {
                 println!("Guessed!!!");
                 break;
             }
-            Ordering::Greater => println!("Too big"),
         }
     }
 }
